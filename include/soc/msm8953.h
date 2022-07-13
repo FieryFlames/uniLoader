0fcadcb390bd8f0292eff311063a599d2b2b4007
Jack Matthews <jm5112356@gmail.com>
2022-07-13 19:19:50 -0400

    soc: qcom: Add basic support for Qualcomm MSM8953 (aka Snapdragon 450)
    Signed-off-by: Jack Matthews <jm5112356@gmail.com>

---
+++ include/soc/msm8953.h
@@ -0,0 +1,10 @@
+
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Jack Matthews <jm5112356@gmail.com>
+ */
+
+#ifndef MSM8953_H_	/* Include guard */
+#define MSM8953_H_
+
+#endif // MSM8953_H_
---
+++ soc/qcom/msm8953.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Jack Matthews <jm5112356@gmail.com>
+ */
+
+#include <soc/msm8953.h>
+
+void soc_init(void) {
+
+}
