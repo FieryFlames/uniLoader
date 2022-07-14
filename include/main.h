9552ed03db49c5cb5ba388d2e5fb09fede4975c0
Jack Matthews <jm5112356@gmail.com>
2022-07-14 09:42:22 -0400

    Revert "main: Use built-in memcpy"
    This reverts commit 3643d6a0c8d935e69689eb9e8c4842377ca3ea89.

--- include/main.h
+++ include/main.h
@@ -3,8 +3,6 @@
  * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
  */
 
-#include <stddef.h>
-
 #ifndef MAIN_H_   /* Include guard */
 #define MAIN_H_
 
@@ -15,35 +13,14 @@ extern void board_init(void);
 
 extern void printk(char *text);
 
-#ifndef __has_builtin
-	#define __has_builtin(x) 0
-#endif
+/* Define our own 128 bit memcpy */
+void memcpy(void *dest, void *src, int size)
+{
+	unsigned __int128 *src2 = src;
+	unsigned __int128 *dest2 = dest;
 
-#if __has_builtin(__builtin_memcpy)
-	void memcpy(void *dest, void *src, size_t size) {
-		__builtin_memcpy(dest, src, size);
-	}
-#else
-	#ifdef __aarch64__
-		/* Define our own 128 bit memcpy */
-		void memcpy(void *dest, void *src, size_t size)
-		{
-			unsigned __int128 *src2 = src;
-			unsigned __int128 *dest2 = dest;
-
-			for (size_t i = 0; i < size / 16; i++)
-				dest2[i] = src2[i];
-		}
-	#endif
-
-	#ifdef __arm__
-		void memcpy (char* src, char* dest, size_t len) {
-			for (size_t i = 0; i < len; i++)
-				dest[i] = src[i];
-		}
-	#endif
-#endif
-
-
+	for (int i=0; i<size/16; i++)
+		dest2[i] = src2[i];
+}
 
 #endif // MAIN_H_
